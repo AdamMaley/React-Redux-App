c65b9d9d5a0eb63ed93730711088024a77ec5ac2
AdamMaley <adamemaley@yahoo.com>
2020-10-20 19:26:00 -0400

    mvp I believe, just trying to think of what else to add.

---
+++ app/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import "./App.css";
+import { Welcome } from './components/Welcome';
+import { useDispatch } from 'react-redux';
+import { loadPokemon } from './reducers/pokemon';
+import { Switch, Route } from 'react-router';
+import { PokemonNotFound } from './components/PokemonNotFound';
+import { Pokemon } from './components/Pokemon';
+
+function App() {
+
+  const [name, setName] = useState();
+  const dispatch = useDispatch();
+
+  return (
+    <div className="center">
+      <div className="pokedex">
+      <Switch>
+        <Route path="/not-found" 
+          exact
+          component={PokemonNotFound} 
+        />
+        <Route path="/pokemon/:pokemonName" 
+          exact
+          component={Pokemon} 
+        />
+        <Route component={Welcome} />
+      </Switch>
+
+      <div className="center-form">
+        <form onSubmit={(e) => {
+          e.preventDefault()
+          dispatch(loadPokemon(name))
+          }}>
+          <input 
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a pokemon name..." />
+          <button type="submit">Search</button>
+        </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
